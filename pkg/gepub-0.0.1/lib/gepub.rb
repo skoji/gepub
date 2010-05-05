076a868ae0145539c9a5dda0f138044c1e56dd82
KOJIMA Satoshi <skoji@mac.com>
2010-05-05 17:41:15 +0900

    modified Manifest.txt

---
+++ pkg/gepub-0.0.1/lib/gepub.rb
@@ -0,0 +1,8 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'gepub/generator'
+
+module GEPUB
+  VERSION = '0.0.1'
+end
