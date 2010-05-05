ed27420cd86427f68d5d5d1f74055d7f6fceb6ce
KOJIMA Satoshi <skoji@mac.com>
2010-05-05 17:36:21 +0900

    removed unnnecessary files

--- pkg/gepub-0.0.1/lib/gepub.rb
+++
@@ -1,8 +0,0 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'gepub/generator'
-
-module GEPUB
-  VERSION = '0.0.1'
-end
