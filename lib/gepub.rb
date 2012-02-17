b06fdaf80d50799c3c8eb50128221a29ef3331f3
KOJIMA Satoshi <skoji@mac.com>
2012-02-17 17:47:15 +0900

    some refactoring

--- lib/gepub.rb
+++ lib/gepub.rb
@@ -1,5 +1,14 @@
+if !({}.respond_to? 'key')
+  class Hash
+    def key(x)
+      index(x)
+    end
+  end
+end
+
 require 'gepub/version'
 require 'gepub/xml_util'
+require 'gepub/meta'
 require 'gepub/metadata'
 require 'gepub/package_data'
 require 'gepub/item'
--- lib/gepub/xml_util.rb
+++ lib/gepub/xml_util.rb
@@ -4,10 +4,10 @@ module GEPUB
     DC_NS = 'http://purl.org/dc/elements/1.1/'
     def prefix(ns)
       prefix = raw_prefix(ns)
-      prefix.nil? ? nil : prefix.sub(/^xmlns:/,'') 
+      prefix.nil? ? nil : prefix.sub(/^xmlns:/,'')
     end
     def raw_prefix(ns)
-      (@namespaces_rev ||= @namespaces.invert)[ns]      
+      @namespaces.key(ns)      
     end
   end
 end
