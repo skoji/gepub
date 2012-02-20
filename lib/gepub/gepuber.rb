b5007719005800313c3e722268d4f3b243dabaf1
KOJIMA Satoshi <skoji@mac.com>
2012-02-21 07:42:26 +0900

    fixed bug in gepbuer... NEED TO WRITE TEST

--- lib/gepub/gepuber.rb
+++ lib/gepub/gepuber.rb
@@ -22,11 +22,11 @@ module GEPUB
     attr_accessor :texts, :resources, :epubname, :coverimg, :table_of_contents, :provider
     
     def method_missing(name, *args)
-      @package.send(name, *args)
+      @book.send(name, *args)
     end
     
     def initialize(param)
-      @package = GEPUB::Package.new()
+      @book = GEPUB::Book.new()
       param.each {
         |k,v|
         self.send "#{k}=", v
@@ -42,22 +42,23 @@ module GEPUB
     def create(destbasedir = ".")
       @provider.new(@texts).each {
         |f, fio|
-        item = add_item(f, fio)
-        @spine << item
-        if !@table_of_contents[f].nil?
-          add_nav(item, @table_of_contents[f]) 
-          @table_of_contents.each {
-            |k,v|
-            k =~ /^#{f}#(.*)$/
-            add_nav(item, v, $1) unless $1.nil?
-          }
-        end
+        @book.ordered {
+          item = add_item(f, fio)
+          if !@table_of_contents[f].nil?
+            item.toc_text table_of_contents[f] 
+            @table_of_contents.each {
+              |k,v|
+              k =~ /^#{f}#(.*)$/
+              add_nav(item, v, $1) unless $1.nil?
+            }
+          end
+        }
       }
 
       @provider.new(@resources).each {
         |f, fio|
         item = add_item(f, fio)
-        specify_cover_image(item) if File.basename(f) == @coverimg
+        item.cover_image if File.basename(f) == @coverimg
       }
       generate_epub(File.join(destbasedir, @epubname + '.epub'))      
     end
