fbdf27b710cc6df90d40d54f33f6f706cc06615c
KOJIMA Satoshi <skoji@mac.com>
2012-02-24 18:51:23 +0900

    README changed.

--- README.md
+++ README.md
@@ -16,7 +16,41 @@ a generic EPUB parser/generator library.
 
 ## SYNOPSIS:
 
-see [examples](https://github.com/skoji/gepub/tree/master/examples/)
+### Builder Example
+
+    require 'rubygem'
+    require 'gepub'
+    workdir = 'epub/example/'
+    builder = GEPUB::Builder.new {
+      unique_identifier 'http:/example.jp/bookid_in_url', 'BookID', 'URL'
+      language 'en'
+
+      title 'GEPUB Sample Book'
+      subtitle 'This book is just a sample'
+      alt 'ja' => 'これはあくまでサンプルです'
+
+      creator 'KOJIMA Satoshi'
+      alt 'ja' => '小嶋智'
+
+      contributors 'Denshobu', 'Asagaya Densho', 'Shonan Densho Teidan', 'eMagazine Torutaru'
+
+      date '2012-02-29T00:00:00Z'
+
+      resources(:workdir => workdir) {
+        cover_image 'img/image1.jpg' => 'image1.jpg'
+        ordered {
+          file 'text/chap1.xhtml'
+          heading 'Chapter 1'
+          file 'text/chap1-1.xhtml'
+          file 'text/chap2.html'
+          heading 'Chapter 2'
+        }
+      }
+    }
+    epubname = File.join(File.dirname(__FILE__), 'example_test_with_builder.epub')
+    builder.generate_epub(epubname)
+
+ [examples](https://github.com/skoji/gepub/tree/master/examples/) 
 
 ## INSTALL:
 
--- examples/builder_example.rb
+++ examples/builder_example.rb
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
-require 'rubygem'
-require 'gepub'
-workdir = 'epub/example/'
-builder = GEPUB::Builder.new {
-  unique_identifier 'http:/example.jp/bookid_in_url', 'BookID', 'URL'
-  language 'en'
+    require 'rubygem'
+    require 'gepub'
+    workdir = 'epub/example/'
+    builder = GEPUB::Builder.new {
+      unique_identifier 'http:/example.jp/bookid_in_url', 'BookID', 'URL'
+      language 'en'
 
-  title 'GEPUB Sample Book'
-  subtitle 'This book is just a sample'
-  alt 'ja' => 'これはあくまでサンプルです'
+      title 'GEPUB Sample Book'
+      subtitle 'This book is just a sample'
+      alt 'ja' => 'これはあくまでサンプルです'
 
-  creator 'KOJIMA Satoshi'
-  alt 'ja' => '小嶋智'
+      creator 'KOJIMA Satoshi'
+      alt 'ja' => '小嶋智'
 
-  contributors 'Denshobu', 'Asagaya Densho', 'Shonan Densho Teidan', 'eMagazine Torutaru'
+      contributors 'Denshobu', 'Asagaya Densho', 'Shonan Densho Teidan', 'eMagazine Torutaru'
 
-  date '2012-02-29T00:00:00Z'
+      date '2012-02-29T00:00:00Z'
 
-  resources(:workdir => workdir) {
-    cover_image 'img/image1.jpg' => 'image1.jpg'
-    ordered {
-      file 'text/chap1.xhtml'
-      heading 'Chapter 1'
-      file 'text/chap1-1.xhtml'
-      file 'text/chap2.html'
-      heading 'Chapter 2'
+      resources(:workdir => workdir) {
+        cover_image 'img/image1.jpg' => 'image1.jpg'
+        ordered {
+          file 'text/chap1.xhtml'
+          heading 'Chapter 1'
+          file 'text/chap1-1.xhtml'
+          file 'text/chap2.html'
+          heading 'Chapter 2'
+        }
+      }
     }
-  }
-}
-epubname = File.join(File.dirname(__FILE__), 'example_test_with_builder.epub')
-builder.generate_epub(epubname)
+    epubname = File.join(File.dirname(__FILE__), 'example_test_with_builder.epub')
+    builder.generate_epub(epubname)
