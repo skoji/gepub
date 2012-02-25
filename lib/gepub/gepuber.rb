63f6b65f9549e5064ee76bcb01945369d71adc81
KOJIMA Satoshi <skoji@mac.com>
2012-02-25 14:29:50 +0900

    removed Gepuber class; executable gepuber now uses Builder. configuration file format has changed.

--- lib/gepub/gepuber.rb
+++ lib/gepub/gepuber.rb
@@ -2,65 +2,40 @@ require 'rubygems'
 
 module GEPUB
   class Gepuber
-    class FileProvider
-      include Enumerable
-      def initialize(pattern)
-        @list = Dir.glob(pattern)
-      end
+    def initialize(&block)
+      @block = block
+    end
 
-      def each (&block)
-        @list.each {
-          |f|
-          File.open(f, 'rb') {
-            |fio|
-            yield f, fio
+    def create
+      table_of_contents = {}
+      epub_name = 'gepuber_generated.epub'
+      GEPUB::Builder.new {
+        block.call
+        resources {
+          Dir.glob('*.css') {
+            |f|
+            file f
+          }
+          Dir.glob('img/*') {
+            |f|
+            if File.basename(f) == 'cover.jpg'
+              cover_image f
+            else
+              file f
+            end
+          }
+
+          ordered {
+            Dir.glob('[0-9]*.{xhtml,html}') {
+              |f|
+              file f
+              if table_of_contents[f]
+                heading table_of_contents[f]
+              end
+            }
           }
         }
-      end
-    end
-
-    attr_accessor :texts, :resources, :epubname, :coverimg, :table_of_contents, :provider
-    
-    def method_missing(name, *args)
-      @book.send(name, *args)
-    end
-    
-    def initialize(param)
-      @book = GEPUB::Book.new()
-      param.each {
-        |k,v|
-        self.send "#{k}=", v
       }
-      @texts ||= ['[0-9]*.{xhtml,html}'] 
-      @resources ||= ['*.css',  'img/*']
-      @coverimg ||= 'cover.jpg'
-      @table_of_contents ||= {}
-      @epubname ||= 'gepuber_generated'
-      @provider ||= FileProvider
-    end
-
-    def create(destbasedir = ".")
-      @provider.new(@texts).each {
-        |f, fio|
-        @book.ordered {
-          item = add_item(f, fio)
-          if !@table_of_contents[f].nil?
-            item.toc_text table_of_contents[f] 
-            @table_of_contents.each {
-              |k,v|
-              k =~ /^#{f}#(.*)$/
-              add_nav(item, v, $1) unless $1.nil?
-            }
-          end
-        }
-      }
-
-      @provider.new(@resources).each {
-        |f, fio|
-        item = add_item(f, fio)
-        item.cover_image if File.basename(f) == @coverimg
-      }
-      generate_epub(File.join(destbasedir, @epubname + '.epub'))      
     end
   end
 end
