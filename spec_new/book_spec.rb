f17d7a0b8d145469a49b46f9eafe92140f2688ab
KOJIMA Satoshi <skoji@mac.com>
2012-08-31 13:56:31 +0900

    create new spec

---
+++ spec_new/book_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+require 'rubygems'
+require 'nokogiri'
+
+describe GEPUB::Book do 
+    context 'on creating new book' do
+        describe 'initialize' do
+        end 
+        describe 'parse' do
+        end
+        describe 'version=' do
+        end
+        describe 'identifer=' do
+        end
+        describe 'add_identifier' do
+        end     
+
+    end    
+end
