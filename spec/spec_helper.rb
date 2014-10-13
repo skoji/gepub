fc64b6f70adb526f493c7c7dba105e0db95f910f
Satoshi KOJIMA <skoji@mac.com>
2014-10-13 13:55:18 +0900

    fix problem on rspec version > 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ end
 
 RSpec.configure do |config|
   # Use color in STDOUT
-  config.color_enabled = true
+  config.color = true
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
   # Use the specified formatter
