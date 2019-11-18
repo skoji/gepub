6e3845d6839da19033238034a174e0886777e3c4
Satoshi Kojima <skoji@skoji.jp>
2019-11-18 20:11:54 +0900

    fix spec issue

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper.rb'
 require 'rubygems'
 require 'nokogiri'
-require 'byebug'
+
 describe GEPUB::Book do
   context 'on creating new book' do
     describe 'initialize' do
